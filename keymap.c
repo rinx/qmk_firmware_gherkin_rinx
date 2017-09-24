7d3f16f4e72db57447f2e635cbc23b34db134c47
Rintaro Okamura <rintaro.okamura@gmail.com>
2017-09-24 18:14:49 +0900

    Fix comma, dot keys / Add tap dance

--- keymap.c
+++ keymap.c
@@ -35,6 +35,11 @@ extern keymap_config_t keymap_config;
 #define MAC_COPY_PASTE 0
 #define WIN_COPY_PASTE 1
 
+// TAP DANCE
+enum {
+    TD_Q_TAB = 0
+}
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -52,7 +57,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,---------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |
+ * |TD_TAB|      |      |      |      |      |      |      |      |      |
  * |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+-------------+------+------+------+------|
  * | Ctrl |      |      |      |      |      |      |      |      | Ctrl |
@@ -63,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
+  TD(TD_Q_TAB), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
   CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, CTL_T(KC_SCLN), \
-  LT(_RAISE, KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, LT(_LOWER, KC_ENT), \
+  LT(_RAISE, KC_Z), KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LT(_LOWER, KC_ENT), \
 ),
 
 /* Lower
@@ -211,6 +216,11 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
 }
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_Q_TAB]  = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_TAB)
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
