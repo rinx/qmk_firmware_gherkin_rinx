c324be63d95c53c88d68ad9e6b62eced9ab7b772
Rintaro Okamura <rintaro.okamura@gmail.com>
2018-01-21 12:36:36 +0900

    Revise default layer to have symmetric mapping

--- keymap.c
+++ keymap.c
@@ -112,26 +112,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |TD_TAB|      |      |      |      |      |      |      |      |      |
  * |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+-------------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      | Ctrl |
+ * | Ctrl |      |      |      |      |      |      |      |      | Ctrl |
  * |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |
  * |------+------+------+------+-------------+------+------+------+------|
- * | Ctrl |  Alt |  GUI | Funct| Lower| Raise| Arrow|      |      | Shift|
+ * | Shift|  Alt |  GUI | Funct| Lower| Raise| Arrow|  GUI |  Alt | Shift|
  * |   Z  |   X  |   C  |   V  | Space|  BS  |   B  |   N  |   M  | Enter|
  * `---------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   TD(TD_Q_TAB), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
-  KC_A, KC_S, KC_D, KC_F, KC_G, \
+  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, \
   KC_H, KC_J, KC_K, KC_L, CTL_T(KC_SCLN), \
-  CTL_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C), LT(_FUNCT, KC_V), LT(_LOWER, KC_SPC), \
-  LT(_RAISE, KC_BSPC), LT(_ARROW, KC_B), KC_N, KC_M, SFT_T(KC_ENT) \
+  SFT_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C), LT(_FUNCT, KC_V), LT(_LOWER, KC_SPC), \
+  LT(_RAISE, KC_BSPC), LT(_ARROW, KC_B), GUI_T(KC_N), ALT_T(KC_M), SFT_T(KC_ENT) \
 ),
 [_RQWERTY] = RKEYMAP( \
   TD(TD_Q_TAB), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
-  KC_A, KC_S, KC_D, KC_F, KC_G, \
+  CTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, \
   KC_H, KC_J, KC_K, KC_L, CTL_T(KC_SCLN), \
-  CTL_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C), LT(_RFUNCT, KC_V), LT(_RLOWER, KC_SPC), \
-  LT(_RRAISE, KC_BSPC), LT(_RARROW, KC_B), KC_N, KC_M, SFT_T(KC_ENT) \
+  SFT_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C), LT(_RFUNCT, KC_V), LT(_RLOWER, KC_SPC), \
+  LT(_RRAISE, KC_BSPC), LT(_RARROW, KC_B), GUI_T(KC_N), ALT_T(KC_M), SFT_T(KC_ENT) \
 ),
 
 /* Lower
